aa40f47473690bc3e60f25585b42adfc841c5977
arg054 <maxim.golovenkov@gmail.com>
2024-10-27 19:01:41 +0100

    game loop, fps control and sprites

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ from constants import *
 
 def main():
     pygame.get_init()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -15,6 +19,7 @@ def main():
                 return
         pygame.Surface.fill(screen, (0, 0, 0, 1))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
